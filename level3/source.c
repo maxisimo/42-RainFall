c49af1bcb31594a569798e99902234062919c9c2
Maxime SIMON <maxisimo@e1r4p2.42.fr>
2020-10-29 17:19:34 +0100

    [ADD] source

---
+++ level3/source.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+int		m;
+
+int		v(void)
+{
+	char	buffer[512];
+
+	fgets(buffer, 512, stdin);
+	printf(buffer);
+	if (m == 64)
+	{
+		fwrite("Wait what?!\n", 12, 1, stdout);
+		system("/bin/sh");
+	}
+	return (0);
+}
+
+int		main(void)
+{
+	v();
+	return (0);
+}
