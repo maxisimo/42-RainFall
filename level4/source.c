5daf5923efbefd384153dce6fe395be1eeb0ce0b
Maxime SIMON <maxisimo@e1r4p2.42.fr>
2020-10-29 17:21:50 +0100

    [ADD] source

---
+++ level4/source.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+int		m;
+
+int		p(char *buffer)
+{
+	printf(buffer);
+	return (0);
+}
+
+int		n(void)
+{
+	char	buffer[512];
+
+	fgets(buffer, 512, stdin);
+	p(buffer);
+	if (m == 16930116)
+		system("/bin/cat /home/user/level5/.pass");
+	return (0);
+}
+
+int		main(void)
+{
+	n();
+	return (0);
+}
